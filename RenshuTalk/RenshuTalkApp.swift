b21e2e576d0c1721f385b903ec71a8e09d0dfc77
Dalvlos Silva <dalvlos@hotmail.com>
2025-07-03 14:04:53 +0900

    Initial Commit

---
+++ RenshuTalk/RenshuTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenshuTalkApp.swift
+//  RenshuTalk
+//
+//  Created by Dalvlos on 2025/07/03.
+//
+
+import SwiftUI
+
+@main
+struct RenshuTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RenshuTalkTests/RenshuTalkTests.swift
@@ -0,0 +1,17 @@
+//
+//  RenshuTalkTests.swift
+//  RenshuTalkTests
+//
+//  Created by Dalvlos on 2025/07/03.
+//
+
+import Testing
+@testable import RenshuTalk
+
+struct RenshuTalkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
