7095b58f44d7d1a76ef42cf5119a611dcbfe4302
Dalvlos Silva <dalvlos@hotmail.com>
2025-08-26 21:07:11 +0900

    bugs corrections

--- RenshuTalk/Models/FraseComAudio.swift
+++ RenshuTalk/Models/FraseComAudio.swift
@@ -7,8 +7,25 @@
 
 import Foundation
 
+struct Recording: Identifiable, Codable {
+    let id: UUID
+    var text: String
+    var audioFileName: String
+    var dateCreated: Date
+}
+
+struct Playlist: Identifiable, Codable {
+    let id: UUID
+    var name: String
+    var recordings: [Recording] // <-- agora existe
+}
+
+struct UserLibrary: Codable {
+    var playlists: [Playlist]
+}
+
 struct FraseComAudio: Identifiable, Codable {
     let id: UUID
-    let texto: String
-    let audioFileName: String
+    var texto: String
+    var audioFileName: String
 }
---
+++ RenshuTalk/ViewModels/PlaylistDetailView.swift
@@ -0,0 +1,7 @@
+//
+//  PlaylistDetailView.swift
+//  RenshuTalk
+//
+//  Created by Dalvlos on 2025/08/12.
+//
+
